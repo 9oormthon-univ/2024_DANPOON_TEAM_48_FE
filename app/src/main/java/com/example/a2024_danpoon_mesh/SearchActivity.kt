0c73241eeae9e5ec1d15888707ee080ccc935327
eastlaw02 <ng524855@gmail.com>
2024-11-20 02:06:36 +0900

    [develop #1] activity_search

---
+++ app/src/main/java/com/example/a2024_danpoon_mesh/SearchActivity.kt
@@ -0,0 +1,19 @@
+package com.example.a2024_danpoon_mesh
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.a2024_danpoon_mesh.databinding.ActivitySearchBinding
+
+class SearchActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
