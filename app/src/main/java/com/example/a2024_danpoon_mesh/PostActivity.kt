344f731e658d825780453649d3ef12ee7dd43e0e
eastlaw02 <ng524855@gmail.com>
2024-11-23 17:44:05 +0900

    [develop #8] activity_post

---
+++ app/src/main/java/com/example/a2024_danpoon_mesh/PostActivity.kt
@@ -0,0 +1,19 @@
+package com.example.a2024_danpoon_mesh
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.a2024_danpoon_mesh.databinding.ActivityPostBinding
+
+class PostActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityPostBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
